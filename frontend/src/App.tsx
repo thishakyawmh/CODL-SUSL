32e8cccca766388fcf3784953b814e49278e12a2
Nithini <nethmakasthuriarachchi@gmail.com>
2026-06-10 12:41:52 +0530

    feat: implement main routing and layout structures with App component configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Outlet, Navigate, useLocation } from 'rea
 // --- Student Portal Imports ---
 import { Sidebar } from './components/student-portal/Sidebar';
 import { Dashboard } from './components/student-portal/Dashboard';
-// [M3] import { CourseDetailsWrapper } from './components/student-portal/CourseDetailsWrapper';
+import { CourseDetailsWrapper } from './components/student-portal/CourseDetailsWrapper';
 // [M3] import { CourseDetails } from './components/student-portal/CourseDetails';
 // [M3] import { CourseExaminations } from './components/student-portal/CourseExaminations';
 // [M3] import { CourseResults } from './components/student-portal/CourseResults';
